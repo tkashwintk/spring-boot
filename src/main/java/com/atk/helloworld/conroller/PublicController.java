ffad8a10efe0f6a67dacbd3924f7689a702dbe41
Ashwin Tk <ashwintk@Ashwins-MacBook-Air.local>
2018-11-10 07:25:43 -0800

    second commit

--- src/main/java/com/atk/helloworld/conroller/PublicController.java
+++ src/main/java/com/atk/helloworld/conroller/PublicController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PublicController {
 	
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String getMessage() {
 		return "Hello Guys";
 	}
